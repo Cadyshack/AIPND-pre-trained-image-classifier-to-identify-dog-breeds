7e561987a350082f6fbd576c952a43406ff6d04a
Christian Cadieux <cadieux_christian@hotmail.com>
2020-10-05 20:32:14 -0400

    added command line arguments

--- intropyproject-classify-pet-images/get_input_args.py
+++ intropyproject-classify-pet-images/get_input_args.py
@@ -38,6 +38,10 @@ def get_input_args():
     Returns:
      parse_args() -data structure that stores the command line arguments object  
     """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir', type = str, default = 'pet_images/', help = 'path to the folder of pet images')
+    parser.add_argument('--arch', type = str, default = 'vgg', help = 'name of CNN model to use')
+    parser.add_argument('--dogfile', type = str, default = 'dognames.txt', help = 'path to the text file with dog names')  
     # Replace None with parser.parse_args() parsed argument collection that 
     # you created with this function 
-    return None
+    return parser.parse_args()
